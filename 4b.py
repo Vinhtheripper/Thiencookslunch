f617cf3a883c832d9c4d275f526f364b7a19fe27
Vinhtheripper <quvinh1305@gmail.com>
2024-11-23 20:08:52 +0700

    Update 4b.py
    sửa

--- 4b.py
+++ 4b.py
@@ -5,3 +5,9 @@ for i in range(1,h+1):
     for k in range(1,i+1):
         print('+',end=' ')
     print()
+for i in range(h-1, 0, -1):
+    for j in range(h - i):
+        print('', end=' ')
+    for k in range(1, i + 1):
+        print('+', end=' ')
+    print()
